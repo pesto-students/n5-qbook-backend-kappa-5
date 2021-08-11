13b97acdbdaf0d0bf12751b06cbe1f4d694ecd5d
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-11 20:18:00 +0530

    add try-catch

--- api/controllers/BookingController.js
+++ api/controllers/BookingController.js
@@ -13,142 +13,237 @@ const pdf = require("html-pdf");
 
 module.exports = {
   checkAvailability: async function (req, res) {
-    const uuid = req.query.uuid;
-    if(!uuid){
-    return res.badRequest({status:false,msg:'please provide correct uuid',data:{}});
-    }
-    const qrCode = await QRCode.findOne({uuid:uuid});
-    if(!qrCode){
-    return res.badRequest({status:false,msg:'uuid is expired already',data:{}});
-    }
-    const Config = await Setting.findOne({ userId: qrCode.userId });
-    const format = "YYYY-MM-DD HH:mm:ss";
-    const currentDate = moment().format("YYYY-MM-DD");
-    const currentTime = moment().format(format);
-    let checktime = false;
-    let finalCheck = false;
-    let message = "";
-    Config.slots &&
-      Config.slots.map((slot) => {
-        let getStart = currentDate + " " + slot.start;
-        let getEnd = currentDate + " " + slot.end;
-        message +=
-          "Start Time:-" + slot.start + " End Time:-" + slot.end + " \n";
-        checktime = moment(currentTime).isBetween(getStart, getEnd);
-        if (checktime) {
-          finalCheck = true;
-        }
-    });
-    if(!finalCheck){
-        return res.badRequest({status:false,msg:'Please book appointment between '+message,data:{}});
-    }
-    const randomValue = Math.floor(Math.random() * 90000) + 10000;
-    return res.ok({status:true,data:{orderId:RazorPayOrderID.id,fees:Config.fees},msg:'"Booking Availabile"'});
+    try {
+      const uuid = req.query.uuid;
+      if (!uuid) {
+        return res.badRequest({
+          status: false,
+          msg: "please provide correct uuid",
+          data: {},
+        });
+      }
+      const qrCode = await QRCode.findOne({ uuid: uuid });
+      if (!qrCode) {
+        return res.badRequest({
+          status: false,
+          msg: "uuid is expired already",
+          data: {},
+        });
+      }
+      const Config = await Setting.findOne({ userId: qrCode.userId });
+      const format = "YYYY-MM-DD HH:mm:ss";
+      const currentDate = moment().format("YYYY-MM-DD");
+      const currentTime = moment().format(format);
+      let checktime = false;
+      let finalCheck = false;
+      let message = "";
+      Config.slots &&
+        Config.slots.map((slot) => {
+          let getStart = currentDate + " " + slot.start;
+          let getEnd = currentDate + " " + slot.end;
+          message +=
+            "Start Time:-" + slot.start + " End Time:-" + slot.end + " \n";
+          checktime = moment(currentTime).isBetween(getStart, getEnd);
+          if (checktime) {
+            finalCheck = true;
+          }
+        });
+      if (!finalCheck) {
+        return res.badRequest({
+          status: false,
+          msg: "Please book appointment between " + message,
+          data: {},
+        });
+      }
+      const randomValue = Math.floor(Math.random() * 90000) + 10000;
+      return res.ok({
+        status: true,
+        data: { orderId: RazorPayOrderID.id, fees: Config.fees },
+        msg: '"Booking Availabile"',
+      });
 
-    return res.ok({
-      data: { orderId: RazorPayOrderID.id },
-      message: '"Booking Availabile"',
-    });
+      return res.ok({
+        data: { orderId: RazorPayOrderID.id },
+        message: '"Booking Availabile"',
+      });
+    } catch (err) {
+      console.log(err);
+      res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
   },
   createNewBooking: async function (req, res) {
-    //validate UUID
-    const uuid = req.body.uuid;
-    if (!uuid) {
-      return res.badRequest("please provide correct uuid");
-    }
-    const qrCode = await QRCode.findOne({ uuid: uuid });
-    if (!qrCode) {
-      return res.badRequest("uuid is expired already");
-    }
-    let CustomerRecord = await Customer.findOne({
-      mobileNum: req.body.mobileNum,
-    });
-    if (!CustomerRecord) {
-      CustomerRecord = await Customer.create({
-        name: req.body.name,
-        mobileNum: req.body.mobileNum,
-        isMobileNumVerified: req.body.isMobileNumVerified,
-        token: req.body.token,
-      }).fetch();
-    }
-
-    if (req.body.paymentMode && req.body.paymentMode == "online") {
-      const validateSignature = crypto
-        .createHmac("hmac_sha256", sails.config.RAZOR_SECRET_KEY)
-        .update(req.body.order_id + "|" + req.body.razorpay_payment_id);
-      if (validateSignature == req.body.razorpay_signature) {
+    try {
+      //validate UUID
+      const uuid = req.body.uuid;
+      if (!uuid) {
+        return res.badRequest("please provide correct uuid");
       }
+      const qrCode = await QRCode.findOne({ uuid: uuid });
+      if (!qrCode) {
+        return res.badRequest("uuid is expired already");
+      }
+      let CustomerRecord = await Customer.findOne({
+        mobileNum: req.body.mobileNum,
+      });
+      if (!CustomerRecord) {
+        CustomerRecord = await Customer.create({
+          name: req.body.name,
+          mobileNum: req.body.mobileNum,
+          isMobileNumVerified: req.body.isMobileNumVerified,
+          token: req.body.token,
+        }).fetch();
+      }
+
+      if (req.body.paymentMode && req.body.paymentMode == "online") {
+        const validateSignature = crypto
+          .createHmac("hmac_sha256", sails.config.RAZOR_SECRET_KEY)
+          .update(req.body.order_id + "|" + req.body.razorpay_payment_id);
+        if (validateSignature == req.body.razorpay_signature) {
+        }
+      }
+
+      let BookingDetail = await Booking.create({
+        customerId: CustomerRecord.id,
+        userId: qrCode.userId,
+        bookingDateTime: new Date().toISOString(),
+        status: 1,
+        paymentMode: req.body.paymentMode,
+        searchToken: crypto.randomBytes(50).toString("hex"),
+        customerInfo: {
+          name: CustomerRecord.name,
+          mobile: CustomerRecord.mobileNum,
+        },
+      }).fetch();
+
+      res.ok({
+        status: true,
+        msg: "booking created successfully",
+        data: BookingDetail,
+      });
+    } catch (err) {
+      console.log(err);
+      res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
     }
+  },
+  createPrescription: async function (req, res) {
+    try {
+      const searchToken = req.body.searchToken;
+      if (!searchToken) {
+      }
+      let BookingDetail = await Booking.findOne({ searchToken: searchToken });
 
-    let BookingDetail = await Booking.create({
-      customerId: CustomerRecord.id,
-      userId: qrCode.userId,
-      bookingDateTime: new Date().toISOString(),
-      status: 1,
-      paymentMode:req.body.paymentMode,
-      searchToken: crypto.randomBytes(50).toString("hex"),
-      customerInfo:{name:CustomerRecord.name,mobile:CustomerRecord.mobileNum}
-    }).fetch();
- 
-    res.ok({status:true,msg:'booking created successfully',data:BookingDetail});
+      //creating pdf from ejs
+      const UserData = await Users.findOne({ id: BookingDetail.userId });
+      const CustomerData = await Customer.findOne({
+        id: BookingDetail.customerId,
+      });
+      const UserConfig = await Setting.findOne({
+        userId: BookingDetail.userId,
+      });
+      const data = {
+        customer: CustomerData,
+        User: UserData,
+        Setting: UserConfig,
+        Prescription: req.body.prescription,
+        Diagnosis: req.body.diagnosis,
+        Mode: BookingDetail.paymentMode,
+      };
 
-
-        
-    },
-    createPrescription: async function(req,res){
-        const searchToken = req.body.searchToken;
-        if(!searchToken){
-
-        }
-        let BookingDetail = await Booking.findOne({searchToken:searchToken});
-        
-        //creating pdf from ejs
-        const UserData = await Users.findOne({id:BookingDetail.userId});
-        const CustomerData = await Customer.findOne({id:BookingDetail.customerId});
-        const UserConfig = await Setting.findOne({userId:BookingDetail.userId});
-        const data = {
-            customer:CustomerData,
-            User:UserData,
-            Setting:UserConfig,
-            Prescription:req.body.prescription,
-            Diagnosis:req.body.diagnosis,
-            Mode:BookingDetail.paymentMode
-        }
-
-        const filePathName = path.resolve('.','views','pages','invoice.ejs');
-        const htmlString = fs.readFileSync(filePathName).toString();
-        let  options = { format: 'Letter', };
-        const ejsData = ejs.render(htmlString, data);
-        //console.log(ejsData);
-        let finalresponse = await pdf.create(ejsData,options).toFile('./assets/uploads/generatedfile.pdf',(err, response) => {
-            if (err) return console.log(err);
-            return response;
+      const filePathName = path.resolve(".", "views", "pages", "invoice.ejs");
+      const htmlString = fs.readFileSync(filePathName).toString();
+      let options = { format: "Letter" };
+      const ejsData = ejs.render(htmlString, data);
+      //console.log(ejsData);
+      let finalresponse = await pdf
+        .create(ejsData, options)
+        .toFile("./assets/uploads/generatedfile.pdf", (err, response) => {
+          if (err) return console.log(err);
+          return response;
         });
-         let fileLoc = await sails.helpers.uploadFile.with({
-             data:'./assets/uploads/generatedfile.pdf'
-         });
-         fs.unlinkSync('./assets/uploads/generatedfile.pdf');
-         let UpdateBooking = await Booking.updateOne({id:BookingDetail.id}).set({diagnosis:req.body.diagnosis,userComment:req.body.prescription,status:2,consultTime:new Date().toISOString(),file:fileLoc.Location});
-         await sails.helpers.sendMessage.with({
-             file:fileLoc.Location,
-             mobile:CustomerData.mobileNum
-         });
-        res.ok({status:true,msg:'Prescription updated successfully',data:UpdateBooking});
-    }, 
+      let fileLoc = await sails.helpers.uploadFile.with({
+        data: "./assets/uploads/generatedfile.pdf",
+      });
+      fs.unlinkSync("./assets/uploads/generatedfile.pdf");
+      let UpdateBooking = await Booking.updateOne({ id: BookingDetail.id }).set(
+        {
+          diagnosis: req.body.diagnosis,
+          userComment: req.body.prescription,
+          status: 2,
+          consultTime: new Date().toISOString(),
+          file: fileLoc.Location,
+        }
+      );
+      await sails.helpers.sendMessage.with({
+        file: fileLoc.Location,
+        mobile: CustomerData.mobileNum,
+      });
+      res.ok({
+        status: true,
+        msg: "Prescription updated successfully",
+        data: UpdateBooking,
+      });
+    } catch (err) {
+      console.log(err);
+      res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
+  },
   BookingListing: async function (req, res) {
-    const user = req.user;
-    let filter = {};
-    const status = parseInt(req.query.status);
-    let bookingList = await Booking.find({ status: status, userId: user.id });
-    res.ok({ status:true,msg:'Booking List successfully',data: bookingList });
+    try {
+      const user = req.user;
+      let filter = {};
+      const status = parseInt(req.query.status);
+      let bookingList = await Booking.find({ status: status, userId: user.id });
+      res.ok({
+        status: true,
+        msg: "Booking List successfully",
+        data: bookingList,
+      });
+    } catch (err) {
+      console.log(err);
+      res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
   },
   BookingDetail: async function (req, res) {
-    const user = req.user;
-    console.log(user);
-    let filter = {};
-    const searchToken = req.query.searchToken;
-    let bookingDetail = await Booking.findOne({ searchToken: searchToken, userId: user.id });
-    let CustomerDetail = await Customer.findOne({id:bookingDetail.customerId});
-    res.ok({ status:true,msg:'Booking Detail',data: {bookingDetail:bookingDetail,customer:CustomerDetail}});
+    try {
+      const user = req.user;
+      console.log(user);
+      let filter = {};
+      const searchToken = req.query.searchToken;
+      let bookingDetail = await Booking.findOne({
+        searchToken: searchToken,
+        userId: user.id,
+      });
+      let CustomerDetail = await Customer.findOne({
+        id: bookingDetail.customerId,
+      });
+      res.ok({
+        status: true,
+        msg: "Booking Detail",
+        data: { bookingDetail: bookingDetail, customer: CustomerDetail },
+      });
+    } catch (err) {
+      console.log(err);
+      res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
   },
 };
--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -9,78 +9,128 @@ const crypto = require("crypto");
 
 module.exports = {
   login: async function (req, res) {
-    var user = await Users.findOne({
-      email: req.param("email"),
-    });
-    //console.log(req.body);
-    if (!user) {
-      user = await sails.helpers.createUser.with(req.body);
-    }
-    // if no errors were thrown, then grant them a new token
-    // set these config vars in config/local.js, or preferably in config/env/production.js as an environment variable
-    var token = jwt.sign({ user: user.id }, sails.config.jwtSecret, {
-      expiresIn: sails.config.jwtExpires,
-    });
-    // set a cookie on the client side that they can't modify unless they sign out (just for web apps)
-    await Users.updateOne({email:user.email}).set({accessToken:token});
-    
-    var data = {
-      result: user,
-      token: token,
-    };
-    // provide the token to the client in case they want to store it locally to use in the header (eg mobile/desktop apps)
-    return res.ok({status:true,msg:'User login successfully',data:data});
-},
+    try {
+      var user = await Users.findOne({
+        email: req.param("email"),
+      });
+      //console.log(req.body);
+      if (!user) {
+        user = await sails.helpers.createUser.with(req.body);
+      }
+      // if no errors were thrown, then grant them a new token
+      // set these config vars in config/local.js, or preferably in config/env/production.js as an environment variable
+      var token = jwt.sign({ user: user.id }, sails.config.jwtSecret, {
+        expiresIn: sails.config.jwtExpires,
+      });
+      // set a cookie on the client side that they can't modify unless they sign out (just for web apps)
+      await Users.updateOne({ email: user.email }).set({ accessToken: token });
 
-updateConfig: async function(req,res){
-   const user = req.user;
-   let setting = await Setting.findOne({userId:user.id});
-   if(!setting){
-     let data = req.body;
-     data.userId = user.id;  
-     setting = await Setting.create(req.body).fetch();
-   }else{
-    setting = await Setting.updateOne({userId:user.id}).set(req.body);
-   } 
-   return res.ok({status:true,msg:'User Setting Details',data:setting});
+      var data = {
+        result: user,
+        token: token,
+      };
+      // provide the token to the client in case they want to store it locally to use in the header (eg mobile/desktop apps)
+      return res.ok({
+        status: true,
+        msg: "User login successfully",
+        data: data,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
+  },
+
+  updateConfig: async function (req, res) {
+    try {
+      const user = req.user;
+      let setting = await Setting.findOne({ userId: user.id });
+      if (!setting) {
+        let data = req.body;
+        data.userId = user.id;
+        setting = await Setting.create(req.body).fetch();
+      } else {
+        setting = await Setting.updateOne({ userId: user.id }).set(req.body);
+      }
+      return res.ok({
+        status: true,
+        msg: "User Setting Details",
+        data: setting,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
   },
 
   dashboard: async function (req, res) {
-    let user = req.user;
-    let setting = {};
-    let detail = await Users.findOne({id:user.id});
-    setting = await Setting.findOne({userId:user.id});
+    try {
+      let user = req.user;
+      let setting = {};
+      let detail = await Users.findOne({ id: user.id });
+      setting = await Setting.findOne({ userId: user.id });
 
-    return res.ok({status:true,msg:'User Dashboard Details',data:{
-        'record':detail,
-        'setting':setting,
-    }});
-},
+      return res.ok({
+        status: true,
+        msg: "User Dashboard Details",
+        data: {
+          record: detail,
+          setting: setting,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
+  },
 
   generateQRCode: async function (req, res) {
-    const user = req.user;
-    let data = {};
-    const hash = crypto.randomBytes(30).toString('hex');
-    let qrCode = await QRCode.findOne({userId:user.id});
-    if(!qrCode){
+    try {
+      const user = req.user;
+      let data = {};
+      const hash = crypto.randomBytes(30).toString("hex");
+      let qrCode = await QRCode.findOne({ userId: user.id });
+      if (!qrCode) {
         data.userId = user.id;
         data.uuid = hash;
-        data.status = 1;   
+        data.status = 1;
         qrCode = await QRCode.create(data).fetch();
-      }else{
-        qrCode = await QRCode.updateOne({userId:user.id}).set({uuid:hash});
-      } 
+      } else {
+        qrCode = await QRCode.updateOne({ userId: user.id }).set({
+          uuid: hash,
+        });
+      }
       const message = {
         data: {
-          score: '850',
-          time: '2:45'
+          score: "850",
+          time: "2:45",
         },
-        token: 'e41rJP7ZwinbWjwqdaSyqw:APA91bGVE03bHzKDw1rMIYdqJp9SJHTiVKanFyv0rzkZBWIBFsjcgxkGv-lIMMZl8KC3G9miivIGrfaoyyJTpjHdwsFyOEfG-tdlgDIBnBKwSAzA61yNJOXuVJU1A2U8JJ52XESjpcFz'
+        token:
+          "e41rJP7ZwinbWjwqdaSyqw:APA91bGVE03bHzKDw1rMIYdqJp9SJHTiVKanFyv0rzkZBWIBFsjcgxkGv-lIMMZl8KC3G9miivIGrfaoyyJTpjHdwsFyOEfG-tdlgDIBnBKwSAzA61yNJOXuVJU1A2U8JJ52XESjpcFz",
       };
       //const notification = await sails.helpers.sendNotification.with(message);
-      
-      const url = sails.config.FRONT_END_URL+'qrcode?uuid='+qrCode.uuid;
-      return res.ok({status:true,msg:'QR Code Url',data:url});
 
-}
+      const url = sails.config.FRONT_END_URL + "qrcode?uuid=" + qrCode.uuid;
+      return res.ok({ status: true, msg: "QR Code Url", data: url });
+    } catch (err) {
+      console.log(err);
+      return res.badRequest({
+        status: false,
+        msg: "Something went wrong !",
+        data: {},
+      });
+    }
+  },
 };
