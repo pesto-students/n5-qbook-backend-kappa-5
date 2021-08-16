262874d108e6796c303b5c0fdd6b1217cdf3f807
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-16 20:48:04 +0530

    add code

--- config/env/production.js
+++ config/env/production.js
@@ -358,7 +358,7 @@ module.exports = {
   jwtExpires: 604800,
   RAZOR_KEY_ID: "rzp_test_Mj02y5458xshqx",
   RAZOR_SECRET_KEY: "tTmy6uCn0zlcRQM3nP7i2Fbd",
-  FRONT_END_URL: "http://localhost:3000/",
+  FRONT_END_URL: "http://www.qbooks.in:3000/",
   AWS_KEY_ID: "AKIAWYXOX7JEZWQ6EU5O",
   AWS_SECRET_KEY_ID: "pCko6rauwfT+Ghu1d3vcU7vwWPHT7cQH/K6ApCQq",
   AWS_BUCKET: "qbook-patient-invoice",
