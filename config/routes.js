bf3a57dbc27935bb790199686a0519af9ac6fda8
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-19 14:39:24 +0530

    Initial Commit

---
+++ config/routes.js
@@ -0,0 +1,48 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+  /***************************************************************************
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
+
+  "/": { view: "pages/homepage" },
+  'GET /api/v1/csrfToken': { action: 'security/grant-csrf-token' },
+  "POST /api/v1/user/login": "UsersController.login",
+  "POST /api/v1/user/updateConfig": "UsersController.updateConfig",
+  "GET /api/v1/user/dashboard": "UsersController.dashboard",
+  "GET /api/v1/user/generate-code": "UsersController.generateQRCode",
+  "GET /api/v1/booking/checkAvailability":
+    "BookingController.checkAvailability",
+  "POST /api/v1/booking/create": "BookingController.createNewBooking",
+  "POST /api/v1/booking/addPrescription":
+    "BookingController.createPrescription",
+  "GET /api/v1/booking/list": "BookingController.BookingListing",
+  "GET /api/v1/booking/detail": "BookingController.BookingDetail",
+  "GET /api/v1/booking/confirmation": "BookingController.bookingConfirmation",
+  "GET /api/v1/booking/cancelAllBooking": "BookingController.CancelledAllBooking",
+  "POST /api/v1/user/support": "UsersController.UserSupportRequest",
+
+  /***************************************************************************
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
+};
