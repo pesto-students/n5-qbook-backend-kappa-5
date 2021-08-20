9837b34d68880f404eb7bf1c57de4b96e2f2f309
Gaurav Tayal <gauravtayal2013@gmail.com>
2021-08-20 22:03:17 +0530

    add code

--- config/cron.js
+++ config/cron.js
@@ -3,7 +3,7 @@ module.exports.cron = {
       schedule: '55 23 * * *',
       onTick: async function() {
         console.log('Cancelling All Appointment which is not entertained!');
-        let bookingList = await Booking.update({status:1}).set({status:3}).fetch();
+        let bookingList = await Booking.update({status:1}).set({status:3,userComment:'Cancel Booking by Cron'}).fetch();
       },
       start: true, 
     }
